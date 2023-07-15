bc5e00e8af3d1a1eaa45b4fb3d72c3fc59775993
robitu <robertoiturralde2009@hotmail.com>
2023-07-15 14:12:58 -0600

    Created header and repository page

---
+++ next_13_crash/app/code/repos/page.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import React from "react";
+import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
+
+async function getRepos() {
+  const response = await fetch("https://api.github.com/users/robitu/repos");
+  const repos = await response.json();
+  return repos;
+}
+
+const Code = async () => {
+  const repos = await getRepos();
+  return (
+    <div className="repos-container">
+      <h2>Repositories</h2>
+      <ul className="repo-list">
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className="repo-details">
+                <span>
+                  <FaStar /> {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch /> {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye /> {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Code;
